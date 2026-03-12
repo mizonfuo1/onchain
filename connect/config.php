47cacadda5735a98b0ad0ffc6ebb10d2f6c9cb8d
Izonfuo Woyengipreye Micheal <jwbinance11@gmail.com>
2026-03-12 11:05:42 +0100

    x

---
+++ connect/config.php
@@ -0,0 +1,12 @@
+<?php 
+
+$host = 'smtp.hostinger.com';  // the domain name .com without https://
+
+$username = 'contact@support-channel.org'; //username of the email account
+
+$password = 'Bybit@12345#'; //password of the email account
+
+$setForm = 'contact@support-channel.org'; //email that is mail is sending from (same as the username);
+
+
+?>
